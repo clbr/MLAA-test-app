44acc4bd85de30addb06637629e4cd6eb75660e8
Lauri Kasanen <cand@gmx.com>
2011-06-06 14:55:24 +0300

    Color1fs: fix y-inversion

--- mlaa/offsetvs.glsl
+++ mlaa/offsetvs.glsl
@@ -4,6 +4,6 @@ void main() {
 	gl_Position = gl_Vertex;
 	gl_TexCoord[0] = gl_MultiTexCoord0;
 
-	offset[0] = gl_MultiTexCoord0.xyxy + PIXEL_SIZE.xyxy * vec4(-1.0, 0.0, 0.0, -1.0);
-	offset[1] = gl_MultiTexCoord0.xyxy + PIXEL_SIZE.xyxy * vec4( 1.0, 0.0, 0.0,  1.0);
+	offset[0] = gl_MultiTexCoord0.xyxy + PIXEL_SIZE.xyxy * vec4(-1.0, 0.0, 0.0,  1.0);
+	offset[1] = gl_MultiTexCoord0.xyxy + PIXEL_SIZE.xyxy * vec4( 1.0, 0.0, 0.0, -1.0);
 }
