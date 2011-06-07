089a0f0b3ec55d66ebd44c4d09b12549f39da784
Lauri Kasanen <cand@gmx.com>
2011-06-07 13:51:30 +0300

    Add shader callback to set multitexturing units

---
+++ shadercb.h
@@ -0,0 +1,11 @@
+class blendcb : public IShaderConstantSetCallBack {
+	public:
+	virtual void OnSetConstants(IMaterialRendererServices* services, s32 udata) {
+		int a = 0;
+		services->setPixelShaderConstant("edgesMapL", (const float*)&a, 1);
+		a = 1;
+		services->setPixelShaderConstant("edgesMap", (const float*)&a, 1);
+		a = 2;
+		services->setPixelShaderConstant("areaMap", (const float*)&a, 1);
+	}
+};
