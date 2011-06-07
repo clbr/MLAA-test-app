7fd42465e8078a8b5abbb9ef8d010d09b56299ae
Lauri Kasanen <cand@gmx.com>
2011-06-07 15:36:32 +0300

    Add help text

--- helpmsg.h
+++ helpmsg.h
@@ -2,6 +2,9 @@
 #define HELPMSG_H
 
 const char helpmsg[] =
-	"";
+	"\n	MLAA test app\n\n"
+	"<app> ../mypic.jpg		Show an image instead of the sphere\n\n"
+	"Uses Jimenez's MLAA. Copyright (C) 2010 by Jorge Jimenez, Belen Masia,\n"
+	"Jose I. Echevarria, Fernando Navarro and Diego Gutierrez.";
 
 #endif
